5e57a2a7826fdf34c384ea19e608e10137a57667
AKrgpt99 <abhinav64@live.com>
2022-06-24 21:50:29 -0400

    added a readme

---
+++ README.md
@@ -0,0 +1,70 @@
+# RTZN React Client
+
+## Dev Dependencies
+
+### All
+
+#### Babel
+
+- @babel/core
+- @babel/cli
+- @babel/preset-env
+- @babel/preset-react
+
+#### Webpack
+
+- webpack
+- webpack-cli
+- webpack-dev-server
+
+#### Loaders
+
+- style-loader
+- css-loader
+- file-loader
+- babel-loader
+
+#### Miscellaneous
+
+- @svgr/webpack
+- assert
+- buffer
+- dotenv
+- dotenv-webpack
+- https-browserify
+- os-browserify
+- stream-browserify
+- stream-http
+- url
+- util
+
+### Install Command
+
+`npm i -D @babel/core @babel/cli @babel/preset-env @babel/preset-react webpack webpack-cli webpack-dev-server style-loader css-loader file-loader babel-loader @svgr/webpack assert buffer dotenv dotenv-webpack https-browserify os-browserify stream-browserify stream-http url util`
+
+## Regular Dependencies
+
+### All
+
+#### React
+
+- react
+- react-dom
+- react-router-dom
+
+#### Redux
+
+- @redux/toolkit
+- react-redux
+
+#### CSS-in-JS
+
+- react-jss
+
+#### Miscellaneous
+
+- clsx
+
+### Install Command
+
+`npm i react react-dom react-router-dom @redux-toolkit react-redux react-jss clsx`
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(a?png|jpe?g|gif|avif|svg|webp|webm|mp4)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    fallback: {
+      fs: false,
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer/"),
+      util: require.resolve("util/"),
+      assert: require.resolve("assert/"),
+      http: require.resolve("stream-http/"),
+      url: require.resolve("url/"),
+      https: require.resolve("https-browserify/"),
+      os: require.resolve("os-browserify/"),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "public"),
+      serveIndex: true,
+    },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ Buffer: ["buffer", "Buffer"] }),
+    new Dotenv(),
+  ],
+};
